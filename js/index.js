ed7db00c028569b30667da235f48e0d3e64ed322
@Ngazz <josephngahu.com@gmail.com>
2023-02-20 16:49:15 +0300

    modification on signup

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+// Log In and Signup Styles
 
+const next = document.getElementById("next");
+const next_items = document.querySelector("main .next ");
 
-
+next.addEventListener('click', () => {
+    next_items.classList.toggle(".next")
+}, true);
